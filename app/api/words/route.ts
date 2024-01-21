80fbc6c8c9744ce764d63984094f4837ec3f918e
panjunqing <panjunqing@ke.com>
2024-01-21 23:11:07 +0800

    Update dictionary

--- app/api/words/route.ts
+++ app/api/words/route.ts
@@ -2,12 +2,11 @@ import list from "../../data/dict.json";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 export async function GET(request: Request) {
-  const numberOfItemsToPick = 10;
+
   const wordlist = [];
-  while (wordlist.length < numberOfItemsToPick && list.length > 0) {
-    const randomIndex = Math.floor(Math.random() * list.length);
-    const pickedItem = list.splice(randomIndex, 1)[0];
-    wordlist.push(pickedItem);
+  for (var i = 0; i < 10; i++) {
+    var randomIndex = Math.floor(Math.random() * list.length);
+    wordlist.push(list[randomIndex]);
   }
 
   return Response.json({ wordlist });
