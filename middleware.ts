2e43d30025c7f15fc8b4a5c4db4f80e14eebb298
panjunqing <panjunqing@ke.com>
2024-05-14 12:53:04 +0800

    Update

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,10 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getToken } from "next-auth/jwt";
-
-
-function signToken(payload: Record<string, unknown>) {
-  console.log("xxxxx")
-  console.log(payload)
-  console.log("xxxxx")
-  var encoded = btoa(JSON.stringify(payload))
-  // var actual = JSON.parse(atob(encoded))
-  return encoded
-}
-
 
 export async function middleware(request: NextRequest) {
-
-  console.log(request.headers)
-  console.log(request.cookies)
-  
-  const session = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET }); 
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('user', signToken(session as Record<string, unknown>))
-
-  const response = NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  })
-
+  // const session = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET }); 
+  // const requestHeaders = new Headers(request.headers)
+  // requestHeaders.set('user', signToken(session as Record<string, unknown>))
+  const response = NextResponse.next()
   return response
 }
