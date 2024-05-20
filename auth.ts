051d10aed854e3220c6224d07c1c3625034cd142
panjunqing <panjunqing@ke.com>
2024-05-20 12:40:31 +0800

    Support email login

--- auth.ts
+++ auth.ts
@@ -2,6 +2,10 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import EmailProvider from "next-auth/providers/email";
+import type { Adapter } from 'next-auth/adapters';
+import { SupabaseAdapter } from "@auth/supabase-adapter"
+
 
 export const handler = NextAuth({
 
@@ -14,7 +18,15 @@ export const handler = NextAuth({
           clientId: process.env.GOOGLE_CLIENT_ID as string,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
+        EmailProvider({
+          server: process.env.EMAIL_SERVER,
+          from: process.env.EMAIL_FROM
+        }),
       ],
+      adapter: SupabaseAdapter({
+        url: process.env.SUPABASE_URL as string,
+        secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+      }) as Adapter,
     
 })
 
