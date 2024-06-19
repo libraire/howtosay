20a273fef47a06dcfb90ad4796a16435c746bb6d
panjunqing <panjunqing@ke.com>
2024-06-19 16:14:59 +0800

    fix build

--- app/components/StarComponent.tsx
+++ app/components/StarComponent.tsx
@@ -28,7 +28,7 @@ export default function StartComponent({ word }: Props) {
             <div className='flex'>
                 Familiarity:
                 {[1, 2, 3, 4, 5].map((item, index) => (
-                    <div className={`${currentLevel == item ? 'bg-amber-300 text-gray-100' : ''} h-[24px] w-[24px] rounded-full text-gray-900 bg-gray-100 hover:text-gray-100 hover:bg-amber-300 relative cursor-pointer text-center mx-1`} onClick={() => { 
+                    <div key={index} className={`${currentLevel == item ? 'bg-amber-300 text-gray-100' : ''} h-[24px] w-[24px] rounded-full text-gray-900 bg-gray-100 hover:text-gray-100 hover:bg-amber-300 relative cursor-pointer text-center mx-1`} onClick={() => { 
                         updateLevel(word, item)
                     }}> {item} </div>
                 ))}
