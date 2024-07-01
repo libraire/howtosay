78541844c749134f5722ecd8ffaa8f7abc6a2cf5
panjunqing <panjunqing@ke.com>
2024-07-01 10:00:00 +0800

    ReAdd Email auth

--- auth.ts
+++ auth.ts
@@ -9,25 +9,25 @@ import { SupabaseAdapter } from "@auth/supabase-adapter"
 
 export const handler = NextAuth({
 
-    providers: [
-        GithubProvider({
-          clientId: process.env.GITHUB_ID as string,
-          clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID as string,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-        // EmailProvider({
-        //   server: process.env.EMAIL_SERVER,
-        //   from: process.env.EMAIL_FROM
-        // }),
-      ],
-      // adapter: SupabaseAdapter({
-      //   url: process.env.SUPABASE_URL as string,
-      //   secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
-      // }) as Adapter,
-    
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  adapter: SupabaseAdapter({
+    url: process.env.SUPABASE_URL as string,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+  }) as Adapter,
+
 })
 
 export const { handlers, auth, signIn, signOut } = handler
