94ec098d369cd23a38bf7964494d69e7ee185d1a
drinking <pan49@126.com>
2025-02-03 23:37:21 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,10 @@ const nextConfig = {
       {
         source: '/hts/api/v1/:path*',
         destination: `${process.env.NEXT_PUBLIC_API_HOST}/hts/api/v1/:path*`
+      },
+      {
+        source: '/auth/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_HOST}/auth/:path*`
       }
     ]
   }
